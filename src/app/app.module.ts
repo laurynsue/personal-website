5f6ac7eda211a346577e93f815bf9d9784e1a6f5
laurynsue <laurynsue88@gmail.com>
2021-10-01 22:29:27 +0800

    updated app component, about component, and home component. added greeting component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,16 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatButtonModule} from '@angular/material/button';
 import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatCardModule} from '@angular/material/card';
 
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { WorkComponent } from './work/work.component';
 import { ContactComponent } from './contact/contact.component';
+import { GreetingComponent } from './greeting/greeting.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +27,8 @@ import { ContactComponent } from './contact/contact.component';
     HomeComponent,
     ExperienceComponent,
     WorkComponent,
-    ContactComponent
+    ContactComponent,
+    GreetingComponent
   ],
   imports: [
     BrowserModule,
@@ -33,6 +38,9 @@ import { ContactComponent } from './contact/contact.component';
     MatButtonModule,
     MatListModule,
     MatIconModule,
+    MatGridListModule,
+    FlexLayoutModule,
+    MatCardModule
     
   ],
   providers: [],
